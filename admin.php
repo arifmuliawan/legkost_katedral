c67d2d6dc6c110a85cbf91fec71c55d0e8f4d424
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-05-18 06:21:33 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -29,7 +29,7 @@ if(isset($action))
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1>User Management</h1>
+                            <h1 style="margin: 18px">User Management</h1>
                         </div>
                     </div>
                 </div><!-- /.container-fluid -->
@@ -40,11 +40,11 @@ if(isset($action))
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col-md-12">
-                            <div class="card">
+                            <div class="card" style="box-shadow: unset;">
                                 <div class="card-header">
                                     <div class="row">
                                         <div class="col-md-10">
-                                            <h3>Admin Table</h3>
+                                            
                                         </div>
                                         <div class="col-md-2">
                                             <?php

9f273b2d01250b1f1e9c22cefbced7f6d9e2c45c
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-05-17 17:32:46 +0700

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,138 @@
+<?php
+include('session.php');
+if($username=='developer')
+{
+    $access_code    ="";
+}
+else
+{
+    $access_code    ="AND access='0'";    
+}
+$query_menu = mysqli_query($con,"SELECT * from menu WHERE parentid='0' AND visible='Y' $access_code order by sortid ASC")or die (mysqli_error($con));
+?>    
+    <div class="wrapper">
+
+        <!-- Main Sidebar Container -->
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <!-- Brand Logo -->
+            <a href="index.php" class="brand-link">
+                <center><span class="brand-text font-weight-light">ADMINISTRATOR</span></center>
+            </a>
+
+            <!-- Sidebar -->
+            <div class="sidebar">
+            <!-- Sidebar user (optional) -->
+            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                <div class="image">
+                    <img src="assets/dist/img/avatar.png" class="img-circle elevation-2" alt="User Image">
+                </div>
+                <div class="info">
+                    <a href="#" class="d-block"><?php echo $user; ?></a>
+                </div>
+            </div>
+
+            <!-- Sidebar Menu -->
+            <nav class="mt-2">
+                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <!-- Add icons to the links using the .nav-icon class
+                    with font-awesome or any other icon font library -->
+                    <?php
+                    while($data_menu=mysqli_fetch_array($query_menu))
+                    {
+                        $id_menu        = $data_menu['id'];
+                        $name_menu      = $data_menu['menu'];
+                        $file_menu      = $data_menu['file'];
+                        $query_submenu  = mysqli_query($con,"SELECT * from menu WHERE parentid='$id_menu' AND visible='Y' $access_code order by sortid ASC")or die (mysqli_error($con));
+                        $sum_submenu    = mysqli_num_rows($query_submenu);
+                        if (in_array($id_menu, $arr_usrmenu))
+                        {
+                            if($sum_submenu<=0)
+                            {    
+                    ?>
+                             <li class="nav-item">
+                                <?php
+                                if(preg_match("/$file_menu/i", $page))
+                                {
+                                    $act_menu    = "active";
+                                }
+                                else
+                                {
+                                    $act_menu    = "";
+                                }
+                                ?>
+                                <a href="index.php?p=<?php echo $file_menu ?>" class="nav-link <?php echo $act_menu ?>">
+                                    <p> <?php echo $name_menu ?> </p>
+                                </a>
+                            </li>
+                    <?php 
+                            }
+                            else
+                            {
+                                if(preg_match("/$file_menu/i", $page)) 
+                                {
+                                    $drop_com   = "menu-is-opening menu-open";
+                                    $disp_com   = "block";
+                                }
+                                else
+                                {
+                                    $drop_com   = "";
+                                    $disp_com   = "none";
+                                }
+                    ?>    
+                                <li class="nav-item <?php echo $drop_com ?>">
+                                    <a href="#" class="nav-link">
+                                    <p>
+                                        <?php echo $name_menu ?>
+                                        <i class="right fas fa-angle-left"></i>
+                                    </p>
+                                    </a>
+                                    <ul class="nav nav-treeview" style="display: <?php echo $disp_com ?>;">
+                                        <?php
+                                        while($data_submenu=mysqli_fetch_array($query_submenu))
+                                        {
+                                            $id_submenu     = $data_submenu['id'];
+                                            $name_submenu   = $data_submenu['menu'];
+                                            $file_submenu   = $data_submenu['file'];
+                                            if (in_array($id_submenu, $arr_usrmenu))
+                                            {
+                                        ?>    
+                                                <li class="nav-item">
+                                                    <?php
+                                                    if($page==$file_submenu)
+                                                    {
+                                                        $act_submenu = "active";
+                                                        $act_bgclr   = 'style="background-color:#432210;color:#ffffff"';
+                                                    }
+                                                    else
+                                                    {
+                                                        $act_submenu = "";
+                                                        $act_bgclr   = "";
+                                                    }
+                                                    ?>
+                                                    <a href="index.php?p=<?php echo $file_submenu ?> " class="nav-link <?php echo $act_submenu ?>" <?php echo $act_bgclr ?>>
+                                                    <i class="far fa-circle nav-icon"></i>
+                                                    <p><?php echo $name_submenu ?></p>
+                                                    </a>
+                                                </li>
+                    <?php 
+                                            }
+                                        }
+                    ?>
+                                    </ul>
+                                </li>
+                    <?php                                                           
+                            }    
+                        }   
+                    }    
+                    ?>
+                    <li class="nav-item">
+                        <a href="index.php?p=logout" class="nav-link">
+                            <p> Logout </p>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <!-- /.sidebar-menu -->
+            </div>
+            <!-- /.sidebar -->
+        </aside>

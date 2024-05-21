eb7361cabeeda30d0617112847a7b4d99f722163
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-05-21 17:31:07 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -110,7 +110,7 @@ if(isset($action))
                                                     $clr_row = "background-color: #ffffff;";
                                                 }
                                         ?>
-                                                <tr style="<?php echo $clr_row ?>" id="<?php echo $sortid ?>">
+                                                <tr style="<?php echo $clr_row ?>" class="tableid" id="<?php echo $sortid ?>">
                                                     <td><?php echo $username ?></td>
                                                     <td><?php echo $password ?></td>
                                                     <td><?php echo $last_login ?></td>

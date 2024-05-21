24bfc426c5d219b3b130a84f7cb91606828fdf2c
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-05-21 16:18:55 +0700

    Update header.php

--- header.php
+++ header.php
@@ -22,8 +22,8 @@
     <script type="text/javascript" src="<?php echo $base_assets ?>ckeditor/ckeditor.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
     <?php
     if(isset($form))

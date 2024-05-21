b1c50aaa26a668bdb728c4c1484ec164e58da66b
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-05-21 16:08:22 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -72,7 +72,7 @@ if(isset($action))
                                             <th>Action</th>
                                         </tr>
                                     </thead>
-                                    <tbody>
+                                    <tbody class="row_position">
                                         <?php
                                         if($sum_query>0)
                                         {
@@ -86,131 +86,6 @@ if(isset($action))
                                                 $last_login = $data_query['last_login'];
                                                 $last_logout= $data_query['last_logout'];
                                                 $visible    = $data_query['visible'];
-                                                if($visible=='Y')
-                                                {
-                                                    $icon_visible   = 'fa fa-unlock';
-                                                    $color_visible  = 'success';
-                                                    $text_visible   = 'Active';
-                                                    $check_visible  = '';
-                                                    $value_visible  = 'N';
-                                                }
-                                                else
-                                                {
-                                                    $icon_visible   = 'fa fa-lock';
-                                                    $color_visible  = 'danger';
-                                                    $text_visible   = 'InActive';
-                                                    $check_visible  = 'checked';
-                                                    $value_visible  = 'Y';
-                                                }
-                                                if ($no % 2 == 0)
-                                                {
-                                                    $clr_row = "background-color: #D9D9D9B2;";
-                                                }
-                                                else
-                                                {
-                                                    $clr_row = "background-color: #ffffff;";
-                                                }
-                                        ?>
-                                                <tr style="<?php echo $clr_row ?>">
-                                                    <td><?php echo $username ?></td>
-                                                    <td><?php echo $password ?></td>
-                                                    <td><?php echo $last_login ?></td>
-                                                    <td><?php echo $last_logout ?></td>
-                                                    <td style="min-width: 130px;display: flex;align-items: center;justify-content: center;">
-                                                        <?php
-                                                        if(in_array(3,$arr_usracc))
-                                                        {
-                                                        ?>    
-                                                            <a href="index.php?p=admin_form&a=3&id=<?php echo $id ?>">
-                                                                <button type="button" class="btn btn-warning"><i class="fa fa-edit"></i></button>
-                                                            </a>
-                                                        <?php
-                                                        }
-                                                        ?>    
-                                                        &nbsp&nbsp&nbsp
-                                                        <?php
-                                                        if(in_array(4,$arr_usracc))
-                                                        {
-                                                        ?>
-                                                            <a href="index.php?p=admin&a=4&id=<?php echo $id ?>" onclick="return confirm('Are you sure you want to delete this item?');">
-                                                                <button type="button" class="btn btn-danger"><i class="fa fa-trash"></i></button>
-                                                            </a>
-                                                        <?php
-                                                        }
-                                                        ?>  
-                                                        <!--<button type="button" class="btn btn-sm btn-<?php echo $color_visible ?>" style="pointer-events: none;"><i class="<?php echo $icon_visible ?>"></i> <?php echo $text_visible ?> </button>-->
-                                                        <?php
-                                                        if(in_array(3,$arr_usracc))
-                                                        {
-                                                            $stt_form   ='';
-                                                        }
-                                                        else
-                                                        {
-                                                            $stt_form   ='disabled';   
-                                                        }    
-                                                        ?>
-                                                        <form id="checkgo" method="post" style="margin-left: 12px;">
-                                                            <input type="hidden" name="id" value="<?php echo $id ?>" id="id<?php echo $id ?>">
-                                                            <input type="hidden" name="tbname" value="admin" id="tbname<?php echo $id ?>">
-                                                            <div class="button r" id="button-1">
-                                                                <input type="checkbox" onchange="doThis<?php echo $id ?>(this)" name="wegeb" id="wegep<?php echo $id ?>" value="<?php echo $value_visible ?>" class="checkbox" <?php echo $check_visible ?>  <?php echo $stt_form ?>/>
-                                                                <div class="knobs"></div>
-                                                                <div class="layer"></div>
-                                                            </div>
-                                                        </form>  
-                                                        <script>
-                                                            function doThis<?php echo $id ?>(checkbox){
-                                                            var wegeb   = document.getElementById("wegep<?php echo $id ?>").value
-                                                            var id      = document.getElementById("id<?php echo $id ?>").value
-                                                            var tbname  = document.getElementById("tbname<?php echo $id ?>").value
-                                                            var info = {wegeb: wegeb,id: id,tbname: tbname};
-                                                            $.ajax({
-                                                                type : "POST",
-                                                                url : "change_visible.php",
-                                                                data : info,
-                                                                success: function(){
-                                                                    document.getElementById("wegep<?php echo $id ?>").value = wegeb == 'Y'?'N':'Y'  
-                                                                },
-                                                                error: function(){
-                                                                     document.getElementById("wegep<?php echo $id ?>").value = wegeb
-                                                                }
-                                                                });
-                                                                return false;
-                                                            }   
-                                                        </script>   
-                                                    </td>
-                                                </tr>
-                                        <?php
-                                                $no++;
-                                            }
-                                        }
-                                        ?>            
-                                    </tbody>
-                                    </table>
-                                    <br><br><br>    
-                                    <table class="table table-bordered">
-                                    <tr>
-                                        <th>Username</th>
-                                        <th>Password</th>
-                                        <th>Last Login</th>
-                                        <th>Last Logout</th>
-                                        <th>Action</th>
-                                    </tr>
-                                    <tbody class="row_position">
-                                    <?php
-                                        if($sum_query2>0)
-                                        {
-                                            $no=1;
-                                            while($data_query2=mysqli_fetch_array($query2))
-                                            {
-                                                $id         = $data_query2['id'];
-                                                $sortid     = $data_query2['sortid'];
-                                                $username   = $data_query2['username'];
-                                                $password   = "**********";
-                                                $access     = $data_query2['access'];
-                                                $last_login = $data_query2['last_login'];
-                                                $last_logout= $data_query2['last_logout'];
-                                                $visible    = $data_query2['visible'];
                                                 if($visible=='Y')
                                                 {
                                                     $icon_visible   = 'fa fa-unlock';
@@ -309,9 +184,9 @@ if(isset($action))
                                                 $no++;
                                             }
                                         }
-                                        ?>
+                                        ?>            
                                     </tbody>
-                                </table>
+                                    </table>
                                 </div>
                                 <!-- /.card-body -->
                             </div>

f32f751ed64083d91d76e07de975a60ec644d081
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-05-21 16:53:04 +0700

    Update header.php

--- header.php
+++ header.php
@@ -23,6 +23,7 @@
     <script type="text/javascript" src="<?php echo $base_assets ?>plugins/jquery/jquery.min.js"></script>
     <script type="text/javascript" src="<?php echo $base_assets ?>plugins/jquery-ui/jquery-ui.min.js"></script>
     <link rel="stylesheet" href="<?php echo $base_assets ?>plugins/jquery-ui/jquery-ui.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js"></script>
     <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>-->
 
     <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">

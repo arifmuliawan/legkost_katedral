ea1a0a0de5f4b47ba72acdc6eea66bf3eb6b96cc
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-05-21 17:55:42 +0700

    Update ajaxPro.php

--- ajaxPro.php
+++ ajaxPro.php
@@ -1,9 +1,4 @@
 <?php 
 $position = $_POST['position'];
-$i=1;
-foreach($position as $k=>$v)
-{
-    $update 	    = mysqli_query($con,"UPDATE admin SET sortid='$i',update_date='$now' WHERE id='$v'")or die('ERROR: '. mysqli_error($con));
-	$i++;
-}
+print_r($position);
 ?>

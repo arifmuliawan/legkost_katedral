032ae377a43ec1d2301403f87f2c94803b963ac9
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-05-21 13:07:37 +0700

    Update change_visible.php

--- change_visible.php
+++ change_visible.php
@@ -22,59 +22,6 @@
             $query_edt  = mysqli_query($con,"SELECT * from menu WHERE id='$id'")or die (mysqli_error($con));
             $data_edt   = mysqli_fetch_array($query_edt);
             $user_edt   = $data_edt['menu'];
-        }    
-        if($tbname=='info_mng')
-        {
-            $query_edt  = mysqli_query($con,"SELECT * from info_mng WHERE id='$id'")or die (mysqli_error($con));
-            $data_edt   = mysqli_fetch_array($query_edt);
-            $user_edt   = $data_edt['title_en'];
-            $code_edt   = $data_edt['code'];
-            if($code_edt==3)
-            {
-                $log_menu   = "Investor - NAV Management";
-            }
-        }
-        if($tbname=='leader_position')
-        {
-            $log_menu   = "Company - Leadership Position Management";
-            $query_edt  = mysqli_query($con,"SELECT * from leader_position WHERE id='$id'")or die (mysqli_error($con));
-            $data_edt   = mysqli_fetch_array($query_edt);
-            $user_edt   = $data_edt['title_en'];
-        }
-        if($tbname=='leader_member')
-        {
-            $log_menu   = "Company - Leadership Member Management";
-            $query_edt  = mysqli_query($con,"SELECT * from leader_member WHERE id='$id'")or die (mysqli_error($con));
-            $data_edt   = mysqli_fetch_array($query_edt);
-            $user_edt   = $data_edt['fullname'];
-        }
-        if($tbname=='document_menu')
-        {
-            $log_menu   = "Document Menu Management";
-            $query_edt  = mysqli_query($con,"SELECT * from document_menu WHERE id='$id'")or die (mysqli_error($con));
-            $data_edt   = mysqli_fetch_array($query_edt);
-            $user_edt   = $data_edt['menu'];
-        }
-        if($tbname=='document_list')
-        {
-            $log_menu   = "Document List Management";
-            $query_edt  = mysqli_query($con,"SELECT * from document_list WHERE id='$id'")or die (mysqli_error($con));
-            $data_edt   = mysqli_fetch_array($query_edt);
-            $user_edt   = $data_edt['title_en'];
-        }
-        if($tbname=='devidend_mng')
-        {
-            $log_menu   = "Investor Management - Devidend Information";
-            $query_edt  = mysqli_query($con,"SELECT * from devidend_mng WHERE id='$id'")or die (mysqli_error($con));
-            $data_edt   = mysqli_fetch_array($query_edt);
-            $user_edt   = $data_edt['fiscal_year'];
-        }
-        if($tbname=='portfolio_mng')
-        {
-            $log_menu   = "Portfolio Management";
-            $query_edt  = mysqli_query($con,"SELECT * from portfolio_mng WHERE id='$id'")or die (mysqli_error($con));
-            $data_edt   = mysqli_fetch_array($query_edt);
-            $user_edt   = $data_edt['client'];
         }
         $log_action = "Edit";
         $log_text   = "Edit visible ".$wegep." - ".$user_edt."  Successfully!";

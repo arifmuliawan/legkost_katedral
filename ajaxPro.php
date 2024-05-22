6b3c00cf703f97028bce1b7d3848f9fdd1fa91da
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-05-22 17:51:32 +0700

    Update ajaxPro.php

--- ajaxPro.php
+++ ajaxPro.php
@@ -1,22 +1,24 @@
 <?php 
 $position = $_POST['position'];
 print_r($position);
-$sql="";
-foreach($position as $pos)
-{
-    $sql    .= "Update admin set sortid=".$pos['sort']." WHERE id=".$pos['id'].";";
-}  
+$sql="";  
 try {
     // First of all, let's begin a transaction
     $con->beginTransaction();
     // A set of queries; if one fails, an exception should be thrown
-    $con->query($sql);
+    foreach($position as $pos)
+    {
+        $sql = "Update admin set sortid=? WHERE id=?";
+        $con->prepare($sql);
+        $con->bind_param("ss",$pos['sortid'],$pos['id']);
+        $con->execute();
+    }
     // If we arrive here, it means that no exception was thrown
     // i.e. no query has failed, and we can commit the transaction
-    $conn->commit();
+    $con->commit();
     } catch (Exception $e) {
     // An exception has been thrown
     // We must rollback the transaction
-    $conn->rollback();
+    $con->rollback();
 }
 ?>

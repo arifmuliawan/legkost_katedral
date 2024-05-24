315d8cfa40b7ac39f1f142560e31872a66141607
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-05-24 18:42:11 +0700

    Update admin_form.php

--- admin_form.php
+++ admin_form.php
@@ -274,7 +274,7 @@ if($action=='3')
                                             ?>
                                         </div>
                                     </div>
-                                    <div class="col-md-1">
+                                    <div class="col-md-1" style="margin-top: 10px;">
                                         <br>
                                         <a href='index.php?p=admin_form&a=3&id=<?php echo $id ?>&f=1'><button type="button" class="btn" style="background-color:#ffffff;color: #88A8D4;font-weight: bold;border-color: #88A8D4;"> <i class="fa fa-undo"></i> </button></a>
                                     </div>

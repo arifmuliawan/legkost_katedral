811eab18a2c3bf6720d2377fb837014e9df05997
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-05-25 18:51:54 +0700

    Update paroki_dewan.php

--- paroki_dewan.php
+++ paroki_dewan.php
@@ -27,23 +27,13 @@
                                     <div class="col-md-12">
                                         <div class="form-group">
                                             <label>FOTO BANNER *</label>
-                                            <fieldset class="upload_dropZone text-center mb-3 p-4">
-
-                                                <legend class="visually-hidden">Image uploader</legend>
-
-                                                <svg class="upload_svg" width="60" height="60" aria-hidden="true">
-                                                <use href="#icon-imageUpload"></use>
-                                                </svg>
-
-                                                <p class="small my-2">Drag &amp; Drop background image(s) inside dashed region<br><i>or</i></p>
-
-                                                <input id="upload_image_background" data-post-name="image_background" data-post-url="https://someplace.com/image/uploads/backgrounds/" class="position-absolute invisible" type="file" multiple accept="image/jpeg, image/png, image/svg+xml" />
-
-                                                <label class="btn btn-upload mb-3" for="upload_image_background">Choose file(s)</label>
-
-                                                <div class="upload_gallery d-flex flex-wrap justify-content-center gap-3 mb-0"></div>
-
-                                            </fieldset>
+                                            <div class="dz-message needsclick">
+                                                Drop files here or click to upload
+                                                <span class="note needsclick">(This is just a demo dropzone. Selected files are <span class="fw-medium">not</span> actually uploaded.)</span>
+                                            </div>
+                                            <div class="fallback">
+                                                <input name="file" type="file" />
+                                            </div>
                                         </div>
                                     </div>
                                 </div>

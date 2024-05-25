57caeac07908a39171501f510d7d03b5aa0ce4bc
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-05-25 18:25:30 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
     <footer class="main-footer">
         <div class="float-right d-none d-sm-block">
         </div>
-        <strong>Copyright &copy; 2023 All rights reserved.
+        <strong>Developed By Designata Studio
     </footer>
 
     <!-- Control Sidebar -->

65add97fef8465204f0ea63fb7c19b2d99a448b8
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-05-26 10:14:32 +0700

    Update paroki_dewan.php

--- paroki_dewan.php
+++ paroki_dewan.php
@@ -1,46 +1,8 @@
 <?php
-if(isset($_POST['submit_banner']))
-{
+if(!empty($_FILES)){
     print_r($_FILES);
     exit();
-    $fullname           = $_POST['fullname'];
-    if($_FILES['photo']['name']!='')
-    {
-        $ekstensi_diperbolehkan = array('png','jpg','jpeg');
-        $nama_photo             = $_FILES['photo']['name'];
-        $x_photo                = explode('.', $nama_photo);
-        $ekstensi_photo         = strtolower(end($x_photo));
-        $ukuran_photo           = $_FILES['photo']['size'];
-        $file_tmp_photo         = $_FILES['photo']['tmp_name'];
-        $file_directory_photo   = "assets/dist/img/company/".$nama_photo;
-        $file_db_photo          = "dist/img/company/".$nama_photo;
-        $photo_info             = getimagesize($file_tmp_photo);
-        $photo_width            = $photo_info[0];
-        $photo_height           = $photo_info[1];
-        if(in_array($ekstensi_photo, $ekstensi_diperbolehkan) === true)
-        {
-            if(($photo_width>='350' && $photo_width<='360') && ($photo_height>='425' && $photo_height<='435'))
-            {          
-                move_uploaded_file($file_tmp_photo, $file_directory_photo);
-                $name_photo     = $file_db_photo; 
-            }
-            else
-            {
-                $error          = 1;
-                $msg_photo      = "Your Resolution image is not compatible";
-            }
-        }    
-        else
-        {
-            $error              = 1;
-            $msg_photo          = "Your image file is not compatible";
-        }
-    }
-    else
-    {
-        $name_photo             = "";
-    }  
-}
+}    
 ?>          
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper" style="background-color: #ffffff;">
@@ -71,6 +33,13 @@ if(isset($_POST['submit_banner']))
                                         <div class="form-group" class="dropzone needsclick" id="dropzone-basic">
                                             <label>FOTO BANNER *</label>
                                             <br>
+                                            <div class="dz-message needsclick">
+                                                <i class="fas fa-upload" style="font-size: 2rem;"></i>
+                                                <div class="mt-3" style="font-weight: bold;">.JPG   .JPEG   .PNG </div>
+                                                <div class="mt-1">Drop files to upload </div>
+                                                <div class="mt-1" style="color: #88A8D4;font-weight: bold;">or Browse Files...</div>
+                                            </div>
+                                            <!--
                                             <div style="margin-bottom: 25px;margin-top: -10px;font-size: 13px;">(1820 x 595 px) JPG/JPEG/PNG</div>
                                             <div class="dropzone dz-clickable border rounded bg-light p-3">
                                                 <div class="dz-default dz-message text-center">
@@ -80,6 +49,7 @@ if(isset($_POST['submit_banner']))
                                                     <div class="mt-1" style="color: #88A8D4;font-weight: bold;">or Browse Files...</div>
                                                 </div>
                                             </div>
+                                            -->
                                         </div>
                                     </div>
                                     <div class="col-md-12">

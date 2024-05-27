7bb69a00a67215207600cf23a87f3ce654054ef0
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-05-27 22:35:48 +0700

    Update paroki_dewan.php

--- paroki_dewan.php
+++ paroki_dewan.php
@@ -18,6 +18,27 @@ else
 {
     $banner_image   = "";
 }    
+if($action==4)
+{
+    $delete_banner  = mysqli_query($con,"UPDATE banner SET url_img='' WHERE id='1' AND code='1'");
+    if($delete_banner==1)
+    {
+        echo "
+            <script type='text/javascript'>
+                $(window).on('load', function() {
+                    $('#deletemodal').modal('show');
+                });
+                var delay = 2000;
+                setTimeout(function(){ window.location ='index.php?p=paroki_dewan'; }, delay);
+            </script>
+        ";
+    }
+    else
+    {
+        echo "GAGAL UPLOAD";
+        exit();
+    }
+}
 ?>
         <div class="modal fade" id="successmodal">
             <div class="modal-dialog">
@@ -26,6 +47,19 @@ else
                         <img src="<?php echo $base_assets ?>dist/img/icon_success.png" style="width: 70px;">
                         <br><br>
                         <h5> Perubahan anda telah berhasil disimpan </h5> 
+                    </div>
+                </div>
+                <!-- /.modal-content -->
+            </div>
+            <!-- /.modal-dialog -->
+        </div>
+        <div class="modal fade" id="deletemodal">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-body" style="text-align: center;vertical-align: middle;padding: 40px;">
+                        <img src="<?php echo $base_assets ?>dist/img/icon_success.png" style="width: 70px;">
+                        <br><br>
+                        <h5> Data Berhasil Dihapus </h5> 
                     </div>
                 </div>
                 <!-- /.modal-content -->
@@ -78,7 +112,7 @@ else
                                     <br>
                                     <button id="uploaderBtnBannerParoki" type="button" class="btn" style="background-color:#88A8D4;color: #ffffff;font-weight: bold;">SAVE</button>
                                     &nbsp&nbsp
-                                    <button type="button" class="btn" style="background-color:#E90000;color: #ffffff;font-weight: bold;">DELETE</button>
+                                    <a href="index.php?p=paroki_dewan&a=4"><button type="button" class="btn" style="background-color:#E90000;color: #ffffff;font-weight: bold;">DELETE</button></a>
                                 </form>
                             </div>
                         <!--</form>-->                    

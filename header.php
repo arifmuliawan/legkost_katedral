cc424ee80dabac098002110243ab8cd41b382487
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-05-27 15:16:31 +0700

    Update header.php

--- header.php
+++ header.php
@@ -25,8 +25,8 @@
     <link rel="stylesheet" href="<?php echo $base_assets ?>plugins/jquery-ui/jquery-ui.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js"></script>
 
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/min/dropzone.min.css" rel="stylesheet">  
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.2.0/min/dropzone.min.js"></script>  
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.css" rel="stylesheet">  
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.js"></script>  
     <!--<link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" />
     <script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>-->
     

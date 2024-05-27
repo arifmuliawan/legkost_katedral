2884bb2711fdc5b56528011a89be54b577bfcf44
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-05-27 21:18:33 +0700

    Update paroki_dewan.php

--- paroki_dewan.php
+++ paroki_dewan.php
@@ -23,29 +23,53 @@
                         <!-- /.card-header -->
                         <!--<form action="" method="post" enctype="multipart/form-data">-->
                             <div class="card-body">
-                            <form id="imageUploadForm">
-                                <div class="form-group">
-                                    <label for="name">Name</label>
-                                    <input type="text" class="form-control" name="name" id="name" aria-describedby="name" placeholder="Enter Name">
-                                    <small class="form-text"></small>
-                                </div>
-                                <div class="form-group">
-                                    <label for="email">Email</label>
-                                    <input type="email" class="form-control" name="email" id="email" placeholder="Email">
-                                    <small class="form-text"></small>
-                                </div>
-                                <div class="form-group">
-                                    <label>FOTO BANNER *</label>
-                                    (1820 x 595 px) JPG/JPEG/PNG
-                                </div>
-                                <div id="imageUpload" class="dropzone"></div>
-                                <button id="uploaderBtn" type="button" class="btn btn-primary">Save</button>
-                            </form>
+                                <form id="imageUploadForm">
+                                    <div class="form-group">
+                                        <label>FOTO BANNER *</label>
+                                        (1820 x 595 px) JPG/JPEG/PNG
+                                    </div>
+                                    <div id="imageUpload" class="dropzone"></div>
+                                    <br>
+                                    <button id="uploaderBtn" type="button" class="btn btn-primary">Save</button>
+                                </form>
                             </div>
                         <!--</form>-->                    
                         </div>
                         <!-- /.card -->
+                    </div>
+                    <!--/.col (left) -->
+                    <div class="col-md-12">
+                        <!-- jquery validation -->
+                        <div class="card" style="box-shadow: unset;">
+                        <!-- /.card-header -->
+                            <form action="" method="post" enctype="multipart/form-data">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label class="form-label">PERIODE JABATAN *</label>
+                                                <input type="text" name="periode_paroki" class="form-control" placeholder="Type something here....">
+                                            </div>
+                                        </div>
+                                    </div>               
+                                </div>
+                                <div class="card-footer" style="background-color: unset;">
+                                <div class="row">
+                                    <div class="col-md-11">
+                                        <table border=0 width="100%">
+                                            <tr>
+                                                <td style="text-align: right;">
+                                                    <button type="submit" class="btn" name="submit" style="background-color:#88A8D4;color: #ffffff;font-weight: bold;">Submit</button>
+                                                </td> 
+                                            </tr>
+                                        </table>
+                                    </div>
+                                </div>        
+                            </div>
+                            </form>                    
                         </div>
+                        <!-- /.card -->
+                    </div>
                     <!--/.col (left) -->
                     <!-- right column -->
                     <div class="col-md-6">

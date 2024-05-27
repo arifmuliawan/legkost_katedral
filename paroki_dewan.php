d35ee6ab39afe82e48e81fd9e0dbdffff243111c
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-05-27 14:58:48 +0700

    Update paroki_dewan.php

--- paroki_dewan.php
+++ paroki_dewan.php
@@ -27,14 +27,14 @@ if(!empty($_FILES)){
                         <div class="card" style="box-shadow: unset;">
                         <!-- /.card-header -->
                         <!--<form action="" method="post" enctype="multipart/form-data">-->
-                        <form action="" enctype="multipart/form-data" class="dropzone" id="image-upload">
+                        <form action="" enctype="multipart/form-data">
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col-md-12">
                                         <div class="file_upload">
                                             <label>FOTO BANNER *</label>
                                             <br>
-                                            <div class="dropzone">  
+                                            <div class="dropzone" id="image-upload">  
                                                 <h3>Upload Multiple Image By Click On Box</h3>  
                                             </div>
                                         </div>

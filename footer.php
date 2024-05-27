81fc19027cddbf975a24dc629da6ed8a28a68825
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-05-27 21:29:05 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -133,7 +133,7 @@
     clickable: true,
     thumbnailWidth:500,
     thumbnailHeight:500,
-    url: '<?php echo $base_current ?>',
+    url: 'ajax_upload.php',
     init: function () {
 
         var myDropzone = this;

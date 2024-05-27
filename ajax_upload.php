60c6605ec2850374fa2a04aa4860d22cbc9ec4db
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-05-27 21:53:35 +0700

    Update ajax_upload.php

--- ajax_upload.php
+++ ajax_upload.php
@@ -1,6 +1,45 @@
 <?php
-print_r($_FILES);
-echo "<br><br>";
-print_r($_POST);
-exit();
+if(isset($_FILES['bannerparoki']))
+{
+    if($_FILES['bannerparoki']['name']!='')
+    {
+        $ekstensi_diperbolehkan  = array('png','jpg','jpeg');
+        $nama_banner             = $_FILES['bannerparoki']['name'];
+        print_r($nama_banner);
+        exit();
+        $x_banner                = explode('.', $nama_banner);
+        $ekstensi_banner         = strtolower(end($x_banner));
+        $ukuran_banner           = $_FILES['bannerparoki']['size'];
+        $file_tmp_banner         = $_FILES['bannerparoki']['tmp_name'];
+        $file_directory_banner   = "assets/dist/img/".$nama_banner;
+        $file_db_banner          = "dist/img/".$nama_banner;
+        $banner_info             = getimagesize($file_tmp_banner);
+        $banner_width            = $banner_info[0];
+        $banner_height           = $banner_info[1];
+        move_uploaded_file($file_tmp_banner, $file_directory_banner);
+        $name_banner     = $file_db_banner;
+    }
+    else
+    {
+        $name_banner             = "";
+    }
+    $update_banner  = mysqli_query($con,"UPDATE banner SET url_img='$name_banner' WHERE id='1' AND code='1'");
+    if($update_banner==1)
+    {
+        echo "
+            <script type='text/javascript'>
+                $(window).on('load', function() {
+                    $('#successmodal').modal('show');
+                });
+                var delay = 2000;
+                setTimeout(function(){ window.location ='index.php?p=paroki_dewan'; }, delay);
+            </script>
+        ";
+    }
+    else
+    {
+        echo "GAGAL UPLOAD";
+        exit();
+    }
+}
 ?>

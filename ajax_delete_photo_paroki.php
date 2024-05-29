d487c9d03abc17a0c491140eb6fd475050291b7b
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-05-29 18:21:47 +0700

    Create ajax_delete_photo_paroki.php

---
+++ ajax_delete_photo_paroki.php
@@ -0,0 +1,21 @@
+<?php
+    include("config.php");
+    $id_paroki      = $_POST['id_paroki'];
+    $delete_photo   = mysqli_query($con,"UPDATE paroki_staff SET url_img='' WHERE id='$id_paroki'")or die (mysqli_error($con));
+    if($delete_photo==1)
+    {
+        $delete_json      = array(
+            'delete_status' => 1,
+            'delete_msg'    => "Delete Success"
+        );
+    }
+    else
+    {
+        $delete_json      = array(
+            'delete_status' => 0,
+            'delete_msg'    => "Delete Failed"
+        );
+    }
+    header("Content-type: application/json; charset=utf-8");
+    return json_encode($delete_json);
+?>

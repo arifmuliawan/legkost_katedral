b93a981ace5b68d7a7ce0971553925ff5253f12e
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-05-29 17:17:04 +0700

    Update paroki_dewan.php

--- paroki_dewan.php
+++ paroki_dewan.php
@@ -438,6 +438,7 @@ if(isset($_POST['submit_periode']))
                                                                     (500 x 500 px) JPG/JPEG/PNG
                                                                 </div>
                                                                 <img src="" id="photo_paroki" width="300px" style="display: none;">
+                                                                <a href="index.php?p=paroki_dewan&a=5" onclick="return confirm('Are you sure you want to delete photo ?')"><button id="btndeletephoto" type="button" class="btn" style="background-color:#E90000;color: #ffffff;font-weight: bold;">DELETE</button></a>
                                                                 <div id="imageUpdateStaffParoki" class="dropzone">
                                                                     <div class="dz-message">
                                                                         <img src="<?php echo $base_assets ?>dist/img/icon_upload.png"><br><br>
@@ -446,6 +447,7 @@ if(isset($_POST['submit_periode']))
                                                                         or <font color='#88A8D4'><b>Browse Files...</b></font>
                                                                     </div>
                                                                 </div>
+                                                                <button id="updateBtnStaffParoki" type="button" class="btn" style="background-color:#88A8D4;color: #ffffff;font-weight: bold;">SAVE</button>
                                                             </div>    
                                                             <div class="col-md-6">
                                                                 <div class="row">
@@ -719,12 +721,16 @@ if(isset($_POST['submit_periode']))
                 if(jdata.url_img!="")
                 {
                     $("#updateparokimodal #photo_paroki").attr('src', jdata.photo_staff).show();
+                    $("#updateparokimodal #btndeletephoto").show();
                     $("#updateparokimodal #imageUpdateStaffParoki").hide();
+                    $("#updateparokimodal #updateBtnStaffParoki").hide();
                 }    
                 else
                 {
                     $("#updateparokimodal #photo_paroki").hide();
+                    $("#updateparokimodal #btndeletephoto").hide();
                     $("#updateparokimodal #imageUpdateStaffParoki").show();
-                }
+                    $("#updateparokimodal #updateBtnStaffParoki").show();
+                    
             });
         </script>

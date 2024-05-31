cd5fd39920b668098e0ca12fa9f01e367b760b0d
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-05-31 18:02:16 +0700

    Update paroki_assets.php

--- ajax/paroki_assets.php
+++ ajax/paroki_assets.php
@@ -31,9 +31,7 @@ if(isset($_FILES['bannerparoki']))
                 $up_img          = move_uploaded_file($file_tmp_banner, $file_directory_banner);
                 if($up_img==1)
                 {
-                    $update_banner  = mysqli_query($con,"UPDATE paroki_asset SET url_img='$name_banner',update_by='$user',update_date='$now' WHERE id='1' AND code='1'") or die (mysqli_error($con));
-                    print_r($update_banner);
-                    exit();
+                    $update_banner  = mysqli_query($con,"UPDATE paroki_asset SET url_img='$name_banner',update_by='$user',update_date='$now' WHERE id='1' AND code='1'");
                     if($update_banner==1)
                     {
                         $response_json       = array(

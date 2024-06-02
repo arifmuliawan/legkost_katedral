d7bef82e702a1e6483f18c764745a6cb6f2b137f
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-06-02 20:02:58 +0700

    Update paroki_dewan.php

--- paroki_dewan.php
+++ paroki_dewan.php
@@ -149,7 +149,7 @@
                                                 'photo_paroki'   => $photo_paroki                                     
                                             );
                                     ?>
-                                            <div class="col-12 col-sm-6 col-md-3 d-flex align-items-stretch flex-column" id="<?php echo $sortid_staff ?>" data-id="<?php echo $id_staff ?>" data-sec="<?php echo $sortid_staff ?>">
+                                            <div class="col-12 col-sm-6 col-md-3 d-flex align-items-stretch flex-column" id="<?php echo $sortid_paroki ?>" data-id="<?php echo $id_paroki ?>" data-sec="<?php echo $sortid_paroki ?>">
                                                 <div class="card bg-light d-flex flex-fill">
                                                     <div class="card-body pt-3">
                                                         <img class="dataparoki" src=<?php echo $photo_paroki ?> width="100%" data-toggle="modal" data-target="#detailparokimodal" data-staff='<?php echo json_encode($paroki_json) ?>'>
@@ -459,19 +459,19 @@
         <!-- END DROPZONE ADD PAROKI LIST -->
         <!-- START CHANGE SORT DEWAN PAROKI -->
         <script type="text/javascript">
-            var parokipos   = document.querySelector('.list-paroki');
-            var sortable    = Sortable.create(parokipos,{
+            var parokilist  = document.querySelector('.list-paroki');
+            var sortable    = Sortable.create(parokilost,{
             onUpdate: function (/**Event*/evt) {
                 arr = [];index=0
                 $('.col-md-2').each(function(item){
                 arr.push({id:$(this).attr('data-id'),sort:index++})
                 })
-                    updateOrderParoki(arr);
+                    updatelistparoki(arr);
                 },
 
             });
             
-            function updateOrderParoki(data) {
+            function updatelistparoki(data) {
                 $.ajax({
                     url:"change_sort.php",
                     type:'post',

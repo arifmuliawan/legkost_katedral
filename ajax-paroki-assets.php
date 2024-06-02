70ebfd3332e5d4f4808f7d197bd5b5b87504126a
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-06-02 08:23:11 +0700

    Update ajax-paroki-assets.php

--- ajax-paroki-assets.php
+++ ajax-paroki-assets.php
@@ -79,20 +79,20 @@ if(isset($_POST['delete_banner']))
     print_r($_POST);
     exit();
     $delete_banner       = mysqli_query($con,"UPDATE paroki_asset SET url_img='',update_by='$user',update_date='$now' WHERE id='1' AND code='1'") or die (mysqli_error($con));
-    if($delete_banner==1)
+    if($delete_banner!=1)
+    {
+        $response_json       = array(
+            'error_status'   => 1,
+            'error_message'  => 'Penghapusan data gagal diproses'
+        );
+    }
+    else
     {
         $file_directory_banner   = "assets/dist/img/paroki/".$_POST['name_banner'];
         unlink($file_directory_banner);
         $response_json       = array(
             'error_status'   => 0,
             'error_message'  => 'Penghapusan data telah berhasil'
-        );
-    }
-    else
-    {
-        $response_json       = array(
-            'error_status'   => 1,
-            'error_message'  => 'Penghapusan data gagal diproses'
         );
     }
 }    

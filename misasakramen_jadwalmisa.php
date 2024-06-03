e28dc1e2ba56ee8eb88f7d1eac2a99d42d1d2874
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-06-03 14:59:24 +0700

    Update misasakramen_jadwalmisa.php

--- misasakramen_jadwalmisa.php
+++ misasakramen_jadwalmisa.php
@@ -16,7 +16,35 @@
                 <div class="container-fluid">
                     <div class="row">
                     <!-- left column -->
-                    
+                    <!-- HANDLE MISA SCHEDULE --->
+                    <div class="col-md-12">
+                        <!-- jquery validation -->
+                        <div class="card" style="">
+                        <!-- /.card-header -->
+                        <!--<form action="" method="post" enctype="multipart/form-data">-->
+                            <div class="card-body">
+                                <div class="row">
+                                <?php
+                                    $query_misa_category    = mysqli_query($con,"SELECT * FROM misa_schedule WHERE parentid='0' AND visible='Y'")or die (mysqli_error($con));
+                                    $sum_misa_category      = mysqli_num_rows($query_misa_category);
+                                    if($sum_misa_category>0)
+                                    {
+                                        while($data_misa_category=mysqli_fetch_array($query_misa_category))
+                                        {
+                                            $misa_category  = $data_misa_category['category_name']; 
+                                ?>
+                                            <div class="col-sm-12">
+                                                <h3 style="margin: 24px"><?php echo $misa_category ?></h3>
+                                            </div>
+                                <?php
+                                        }
+                                    }
+                                ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /HANDLE MISA SCHEDULE --->
                 </div>
             </section>  
         </div>

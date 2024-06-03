d8628c77e0743791a30ec3d6e39190b933da0815
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-06-03 12:09:38 +0700

    Update ajax-paroki-assets.php

--- ajax-paroki-assets.php
+++ ajax-paroki-assets.php
@@ -2,8 +2,6 @@
 include("config.php");
 include("session.php");
 //include("session.php");
-print_r($_POST);
-exit();
 if(isset($_FILES['bannerparoki']))
 {
     if($_FILES['bannerparoki']['name']!='')

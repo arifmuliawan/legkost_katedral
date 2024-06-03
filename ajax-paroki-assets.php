616c26fc650b4271b852e77cd740c29d8c3aeacf
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-06-03 12:15:21 +0700

    Update ajax-paroki-assets.php

--- ajax-paroki-assets.php
+++ ajax-paroki-assets.php
@@ -349,8 +349,6 @@ if(isset($_FILES['update_photo_paroki']))
 
 if(isset($_POST['edit_paroki']))
 {
-    print_r($_POST);
-    exit();
     $id_paroki      = $_POST['id_paroki'];
     $name_paroki    = $_POST['name_paroki'];
     $position_paroki= $_POST['position_paroki'];
@@ -401,7 +399,6 @@ if(isset($_POST['edit_paroki']))
             }
             else
             {
-                unlink($photo_paroki);
                 $response_json       = array(
                     'error_status'   => 0,
                     'error_message'  => 'perubahan data berhasil disimpan'

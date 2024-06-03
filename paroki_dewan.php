31886aa6a41f406284dff405331ef4821dcaa578
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-06-03 13:40:23 +0700

    Update paroki_dewan.php

--- paroki_dewan.php
+++ paroki_dewan.php
@@ -722,6 +722,7 @@
                     {
                         toastr['success'](data.error_message);
                         var delay = 3000;
+                        setTimeout(function(){ window.location ='index.php?p=paroki_dewan'; }, delay);
                     }
                     console.log(data,status);
                 }

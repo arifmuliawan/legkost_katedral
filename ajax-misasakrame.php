e312a08ec8e0bf3870aab6368ba53a9bf99970bd
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-06-08 13:16:53 +0700

    Update ajax-misasakrame.php

--- ajax-misasakrame.php
+++ ajax-misasakrame.php
@@ -19,7 +19,7 @@ if(isset($_POST['reset_schedule_misa']))
     else
     {
         $data_reset          = '00:00:0:0|00:00:0:0|00:00:0:0|00:00:0:0|00:00:0:0|00:00:0:0|00:00:0:0|00:00:0:0';
-        $reset_schedule      = mysqli_query($con,"UPDATE misa_schedule SET schedule='$data_reset' WHERE id='$scheduleid'") or die (mysqli_error($con));
+        $reset_schedule      = mysqli_query($con,"UPDATE misa_schedule SET schedule='$data_reset',update_by='$user', update_date='$now' WHERE id='$scheduleid'") or die (mysqli_error($con));
         if($reset_schedule!=1)
         {
             $response_json       = array(
@@ -35,6 +35,51 @@ if(isset($_POST['reset_schedule_misa']))
             );
         }
     } 
+}
+if(isset($_POST['publish_misakhusus']))
+{
+    $misakhususid   = $_POST['misakhususid'];
+    $pubish_start   = $_POST['publish_start'];
+    $pubish_end     = $_POST['publish_end'];
+    if($pubish_start=="" && $pubish_end=="")
+    {
+        $response_json       = array(
+            'error_status'   => 1,
+            'error_message'  => 'Tanggal Mulai & Tanggal Berakhir Harus Terisi'
+        );
+    }
+    elseif($pubish_start=="" && $pubish_end!="")
+    {
+        $response_json       = array(
+            'error_status'   => 1,
+            'error_message'  => 'Tanggal Mulai Harus Terisi'
+        );
+    }
+    elseif($pubish_start!="" && $pubish_end=="")
+    {
+        $response_json       = array(
+            'error_status'   => 1,
+            'error_message'  => 'Tanggal Berakhir Harus Terisi'
+        );
+    }
+    else
+    {
+        $update_misakhusus  = mysqli_query($con,"UPDATE misa_khusus SET publish_start='$pubish_start', publish_end='$pubish_end', update_by='$user', update_date='$now' WHERE id='$misakhususid'") or die (mysqli_error($con));
+        if($update_misakhusus!=1)
+        {
+            $response_json       = array(
+                'error_status'   => 1,
+                'error_message'  => 'Perubahan data gagal disimpan'
+            );
+        }
+        else
+        {
+            $response_json       = array(
+                'error_status'   => 0,
+                'error_message'  => 'Perubahan data berhasil disimpan'
+            ); 
+        }
+    }
 }
 
 header("Content-type: application/json; charset=utf-8");

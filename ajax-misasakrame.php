20945b70b0fa1799710cf30bfc45f95bc9064686
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-06-08 21:57:07 +0700

    Update ajax-misasakrame.php

--- ajax-misasakrame.php
+++ ajax-misasakrame.php
@@ -97,7 +97,7 @@ if(isset($_POST['detail_misakhusus']))
     $misakhusustitle        = $_POST['misakhusustitle'];
     $misakhususdesc         = $_POST['misakhususdesc'];
     $misakregisurl          = $_POST['misakregisurl'];
-    print_r($_POST['misakhususid']);
+    print_r($_POST['misakhusustitle']);
     exit();
     if($misakhusustitle=="")
     {

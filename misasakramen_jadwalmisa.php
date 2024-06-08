e8e9e893cb6c247fa01a419a16be110aa48a0c1e
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-06-08 07:23:14 +0700

    Update misasakramen_jadwalmisa.php

--- misasakramen_jadwalmisa.php
+++ misasakramen_jadwalmisa.php
@@ -20,7 +20,7 @@ if(isset($_POST['updateschedule']))
     {
 ?> 
         <script>        
-            notifmodal(0,'success');
+            notifmodal('Perubahan data telah berhasil disimpan','success');
         </script> 
 <?php
     }

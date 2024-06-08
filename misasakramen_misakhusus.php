a295046e91b615216667507210baa57814afbeb7
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-06-08 21:44:25 +0700

    Update misasakramen_misakhusus.php

--- misasakramen_misakhusus.php
+++ misasakramen_misakhusus.php
@@ -203,7 +203,7 @@ else
                 {
                     misakhususid:$("#formdetail input[name=misakhususid]").val(),
                     misakhusustitle:$("#formdetail input[name=title]").val(),
-                    misakhususdesc:document.getElementById("#editordesc"),
+                    misakhususdesc:CKEDITOR.instances['editordesc'].getData();,
                     misakregisurl:$("#formdetail input[name=regis_url]").val(),
                     detail_misakhusus:true
                 },

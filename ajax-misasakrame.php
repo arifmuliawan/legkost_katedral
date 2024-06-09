cb95f6211dbfe1b6540e3bd1a169570a04944aeb
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-06-09 10:32:10 +0700

    Update ajax-misasakrame.php

--- ajax-misasakrame.php
+++ ajax-misasakrame.php
@@ -158,7 +158,7 @@ if(isset($_FILES['upload_kregisimg']))
         $image_info             = getimagesize($file_tmp_image);
         $image_width            = $image_info[0];
         $image_height           = $image_info[1];
-        if(file_exists("assets/dist/img/paroki/".$nama_image))
+        if(file_exists("assets/dist/img/misa/".$nama_image))
         {
             http_response_code(410);
             $response_json       = array(
@@ -212,7 +212,79 @@ if(isset($_FILES['upload_kregisimg']))
             }    
         }    
     }
-}    
+} 
+
+if(isset($_FILES['upload_kscheduleimg']))
+{
+    $misakhususid               = 1;
+    if($_FILES['upload_kscheduleimg']['name'][0]!='')
+    {
+        $ekstensi_diperbolehkan = array('png','jpg','jpeg');
+        $nama_image             = $_FILES['upload_kscheduleimg']['name'][0];
+        $x_image                = explode('.', $nama_image);
+        $ekstensi_image         = strtolower(end($x_image));
+        $ukuran_image           = $_FILES['upload_kscheduleimg']['size'][0];
+        $file_tmp_image         = $_FILES['upload_kscheduleimg']['tmp_name'][0];
+        $file_directory_image   = "assets/dist/img/misa/".$nama_image;
+        $file_db_image          = "dist/img/misa/".$nama_image;
+        $image_info             = getimagesize($file_tmp_image);
+        $image_width            = $image_info[0];
+        $image_height           = $image_info[1];
+        if(file_exists("assets/dist/img/misa/".$nama_image))
+        {
+            http_response_code(410);
+            $response_json       = array(
+                'error_status'   => 1,
+                'error_message'  => 'Nama file sudah digunakan, silahkan upload kembali dengan nama file berbeda'
+            );
+        }
+        else
+        {
+            if(($image_width<='190' && $image_width>='200') && ($image_height<='190' && $image_height>='200'))
+            {
+                http_response_code(410);
+                $response_json       = array(
+                    'error_status'   => 1,
+                    'error_message'  => 'Resolusi Gambar Tidak Sesuai (500 X 500)'
+                );
+            }
+            else
+            {
+                $upload_file   = @move_uploaded_file($file_tmp_image, $file_directory_image);
+                if($upload_file===false)
+                {
+                    http_response_code(410);
+                        $response_json       = array(
+                        'error_status'   => 1,
+                        'error_message'  => 'Gambar gagal di upload ke server'
+                    );
+                    return;
+                }
+                else
+                {
+                    $update_misakhusus  = mysqli_query($con,"UPDATE misa_khusus SET schedule_img='$file_db_image', update_by='$user', update_date='$now' WHERE id='$misakhususid'") or die (mysqli_error($con));
+                    if($update_misakhusus!=1)
+                    {
+                        unlink($file_directory_image);
+                        http_response_code(410);
+                            $response_json   = array(
+                            'error_status'   => 1,
+                            'error_message'  => 'Gambar gagal di upload ke server'
+                        );
+                    }
+                    else
+                    {
+                        $response_json       = array(
+                            'error_status'   => 0,
+                            'error_message'  => 'Penambahan data telah berhasil disimpan',
+                            'kregis_img'     => $base_assets.$file_db_image
+                        );
+                    }
+                }    
+            }    
+        }    
+    }
+} 
 
 header("Content-type: application/json; charset=utf-8");
 echo json_encode($response_json);

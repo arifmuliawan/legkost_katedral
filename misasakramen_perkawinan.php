7497d5631e4191b5607437870302da34c3f9f2a7
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-06-15 13:31:20 +0700

    Create misasakramen_perkawinan.php

---
+++ misasakramen_perkawinan.php
@@ -0,0 +1,18 @@
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper" style="background-color: #ffffff;">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 style="margin: 24px">PENGUMUMAN PERKAWINAN</h1>
+                            <p style="margin: 24px;color: red;font-size: 14px;">KOLOM DENGAN TANDA * WAJIB DIISI</p>
+                            <p style="margin: 24px;font-size: 14px;">
+                                Pengisian data dari Pengumuman Pertama. Seminggu dari Pengumuman Pertama, data otomatis akan berpindah menjadi Pengumuman Kedua. Seminggu kemudian, otomatis berpindah menjadi Pengumuman Ketiga. Dan seminggu kemudian akan otomatis hilang (archive).
+                            </p>
+                        </div>
+                    </div>
+                </div><!-- /.container-fluid -->
+            </section>
+                       
+        </div> 

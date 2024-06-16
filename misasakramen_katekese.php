50f4f8086d7e790723686051087e25c508128fd3
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-06-16 10:34:31 +0700

    Update misasakramen_katekese.php

--- misasakramen_katekese.php
+++ misasakramen_katekese.php
@@ -8,9 +8,11 @@
                             <h1 style="margin: 24px">KATEKESE</h1>
                         </div>
                         <div class="col-sm-12" style="text-align:right">
-                            <button id="btnaddlist" type="button" class="btn-sm" style="margin: 24px;background-color:#88A8D4;color: #ffffff;font-weight: bold;display: inline-block;text-align: center;vertical-align: middle;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;padding: .375rem .75rem;font-size: 1rem;line-height: 1.5;border-radius: .25rem;transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;border: unset;">
-                                Add New
-                            </button>
+                            <a href="index.php?p=misasakramen_formkatekese">
+                                <button type="button" class="btn-sm" style="margin: 24px;background-color:#88A8D4;color: #ffffff;font-weight: bold;display: inline-block;text-align: center;vertical-align: middle;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;padding: .375rem .75rem;font-size: 1rem;line-height: 1.5;border-radius: .25rem;transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;border: unset;">
+                                    Add New
+                                </button>
+                            </a>
                         </div>
                     </div>
                 </div><!-- /.container-fluid -->

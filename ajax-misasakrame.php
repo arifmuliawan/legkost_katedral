73dedededc6b51851cf685293f400813fb18f01f
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-06-16 09:00:19 +0700

    Update ajax-misasakrame.php

--- ajax-misasakrame.php
+++ ajax-misasakrame.php
@@ -411,6 +411,37 @@ if(isset($_POST['add_perkawinan']))
     }
 }
 
+if(isset($_POST['edit_perkawinan']))
+{
+    $id                 = $_POST['id'];
+    $nama_pria          = $_POST['nama_pria'];
+    $paroki_pria        = $_POST['paroki_pria'];
+    $nama_wanita        = $_POST['nama_wanita'];
+    $paroki_wanita      = $_POST['paroki_wanita'];
+    $input_publish_start= $_POST['publish_start'];
+    $exp_publish_start  = explode("/",$input_publish_start);
+    $ds                 = $exp_publish_start[0];
+    $ms                 = $exp_publish_start[1];
+    $ys                 = $exp_publish_start[2];
+    $publish_start      = $ys.'-'.$ms.'-'.$ds;
+    $update_perkawinan  = mysqli_query($con,"UPDATE `perkawinan_list` SET `nama_pria`='$nama_pria',`paroki_pria`='$paroki_pria',`nama_wanita`='$nama_wanita',`paroki_wanita`='$paroki_wanita',`pengumuman`='$publish_start',`update_by`='$user',`update_date`='$now' WHERE id='$id'")or die (mysqli_error($con));
+    if($update_perkawinan!=1)
+    {
+        http_response_code(410);
+        $response_json       = array(
+            'error_status'   => 1,
+            'error_message'  => 'Perubahan data gagal disimpan'
+        );
+    }
+    else
+    {
+        $response_json       = array(
+            'error_status'   => 0,
+            'error_message'  => 'Perubahan data berhasil disimpan'
+        );
+    }
+}
+
 header("Content-type: application/json; charset=utf-8");
 echo json_encode($response_json);
 ?>

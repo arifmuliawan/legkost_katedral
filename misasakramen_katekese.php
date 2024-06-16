1344b7ed83cd38f4ebeaa7d8da087f7c458e32df
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-06-16 10:17:05 +0700

    Create misasakramen_katekese.php

---
+++ misasakramen_katekese.php
@@ -0,0 +1,18 @@
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper" style="background-color: #ffffff;">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-12">
+                            <h1 style="margin: 24px">KATEKESE</h1>
+                        </div>
+                        <div class="col-sm-12" style="text-align:right">
+                            <button id="btnaddlist" type="button" class="btn-sm" style="margin: 24px;background-color:#88A8D4;color: #ffffff;font-weight: bold;display: inline-block;text-align: center;vertical-align: middle;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;padding: .375rem .75rem;font-size: 1rem;line-height: 1.5;border-radius: .25rem;transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;border: unset;">
+                                Add New
+                            </button>
+                        </div>
+                    </div>
+                </div><!-- /.container-fluid -->
+            </section> 
+        </div>

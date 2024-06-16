febf6d80b73785e2f29a39b7cd1f8b572fb8d768
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-06-16 10:36:42 +0700

    Update misasakramen_katekese.php

--- misasakramen_katekese.php
+++ misasakramen_katekese.php
@@ -7,13 +7,6 @@
                         <div class="col-sm-12">
                             <h1 style="margin: 24px">KATEKESE</h1>
                         </div>
-                        <div class="col-sm-12" style="text-align:right">
-                            <a href="index.php?p=misasakramen_katekese_form">
-                                <button type="button" class="btn-sm" style="margin: 24px;background-color:#88A8D4;color: #ffffff;font-weight: bold;display: inline-block;text-align: center;vertical-align: middle;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;padding: .375rem .75rem;font-size: 1rem;line-height: 1.5;border-radius: .25rem;transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;border: unset;">
-                                    Add New
-                                </button>
-                            </a>
-                        </div>
                     </div>
                 </div><!-- /.container-fluid -->
             </section> 
@@ -22,6 +15,13 @@
             <section class="content">
                 <div class="container-fluid">
                     <div class="row">
+                        <div class="col-sm-12" style="text-align:right">
+                            <a href="index.php?p=misasakramen_katekese_form">
+                                <button type="button" class="btn-sm" style="margin: 24px;background-color:#88A8D4;color: #ffffff;font-weight: bold;display: inline-block;text-align: center;vertical-align: middle;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;padding: .375rem .75rem;font-size: 1rem;line-height: 1.5;border-radius: .25rem;transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;border: unset;">
+                                    Add New
+                                </button>
+                            </a>
+                        </div>
                         <!-- left column -->
                         <div class="col-md-12" style="flex: unset;margin-left: 50px;">
                             <table width="100%">

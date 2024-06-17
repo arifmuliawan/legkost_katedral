1f4726c80a545b5f23eec5ab15f0b28d9cb464f5
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-06-17 21:05:09 +0700

    Update misasakramen_katekeseform.php

--- misasakramen_katekeseform.php
+++ misasakramen_katekeseform.php
@@ -525,7 +525,7 @@
                         else
                         {
                             notifmodal(data.error_message,'success');
-                            setTimeout(function(){ window.location ='index.php?p=misasakramen_katekese';3000});
+                            //setTimeout(function(){ window.location ='index.php?p=misasakramen_katekese';3000});
                         }
                         console.log(data,status);
                     }

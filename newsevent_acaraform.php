1001a5797dd6ab8f676b69d7c0428dc4df52d13d
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-06-18 06:54:59 +0700

    Update newsevent_acaraform.php

--- newsevent_acaraform.php
+++ newsevent_acaraform.php
@@ -203,7 +203,7 @@
                                     ?>
                                             <div class="col-md-4">
                                                 <img id="gallery_img" src="<?php echo $base_assets.$img_gallery ?>" width="100%">
-                                                <button class="btndeletegallery" data-gallery='<?php echo json_encode($gallery_json) ?>' type="button" style="margin: 24px;background-color:#E90000;color: #ffffff;font-weight: bold;display: inline-block;text-align: center;vertical-align: middle;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;padding: .375rem .75rem;font-size: 1rem;line-height: 1.5;border-radius: .25rem;transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;border: unset;">
+                                                <button class="btndeletegallery" data-gallery='<?php echo json_encode($gallery_json) ?>' type="button" style="margin: 24px 0px;background-color:#E90000;color: #ffffff;font-weight: bold;display: inline-block;text-align: center;vertical-align: middle;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;padding: .375rem .75rem;font-size: 1rem;line-height: 1.5;border-radius: .25rem;transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;border: unset;">
                                                     DELETE
                                                 </button>
                                             </div>

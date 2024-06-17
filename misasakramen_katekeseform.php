691aeb3477b6f460043783156cb045477455a372
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-06-17 14:02:34 +0700

    Update misasakramen_katekeseform.php

--- misasakramen_katekeseform.php
+++ misasakramen_katekeseform.php
@@ -272,7 +272,7 @@
         
         <!-- START CANCEL REPLACE THUMBNAIL -->
         <script>
-            $("#katekeseform #btnreplacethumb").click(function()
+            $("#katekeseform #btncancelthumb").click(function()
             {
                 $("#katekeseform #uploadthumb").hide();
                 $("#katekeseform #btnuploadthumb").hide();

8738eba6e9bff5d47bfe73107ccfff89545630cf
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-06-24 16:21:35 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,9 +1,10 @@
+    <!--
     <footer class="main-footer">
         <div class="float-right d-none d-sm-block">
         </div>
         <strong>Developed By Designata Studio
     </footer>
-
+    -->
     <!-- Control Sidebar -->
     <aside class="control-sidebar control-sidebar-dark">
         <!-- Control sidebar content goes here -->

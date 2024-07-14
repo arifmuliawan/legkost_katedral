670485aa93fe33b9790c8f02ccc4d83dd3ea9aa5
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-07-14 15:17:44 +0700

    Update newsevent_acara.php

--- newsevent_acara.php
+++ newsevent_acara.php
@@ -18,6 +18,9 @@
                         <div class="col-sm-12" style="text-align:right">
                             <table width="100%">
                                 <tr>
+                                    <td>
+                                    = Berita Utama
+                                    </td>
                                     <td align="right">
                                         <a href="index.php?p=newsevent_acaraform&id=0">
                                         <button type="button" class="btn-sm" style="margin: 24px;background-color:#88A8D4;color: #ffffff;font-weight: bold;display: inline-block;text-align: center;vertical-align: middle;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;padding: .375rem .75rem;font-size: 1rem;line-height: 1.5;border-radius: .25rem;transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;border: unset;">

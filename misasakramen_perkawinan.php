4e60df093bd2318d64aeb92735ab11ce507c5b1c
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-07-14 12:10:08 +0700

    Update misasakramen_perkawinan.php

--- misasakramen_perkawinan.php
+++ misasakramen_perkawinan.php
@@ -6,8 +6,8 @@
                     <div class="row mb-2">
                         <div class="col-sm-12">
                             <h1 style="margin: 24px">PENGUMUMAN PERKAWINAN</h1>
-                            <p style="margin: 24px;color: red;font-size: 14px;">KOLOM DENGAN TANDA * WAJIB DIISI</p>
-                            <p style="margin: 24px;font-size: 14px;">
+                            <p style="margin: 24px;color: red;">KOLOM DENGAN TANDA * WAJIB DIISI</p>
+                            <p style="margin: 24px;">
                                 Pengisian data dari Pengumuman Pertama. Seminggu dari Pengumuman Pertama, data otomatis akan berpindah menjadi Pengumuman Kedua. Seminggu kemudian, otomatis berpindah menjadi Pengumuman Ketiga. Dan seminggu kemudian akan otomatis hilang (archive).
                             </p>
                         </div>
